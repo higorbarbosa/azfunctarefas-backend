1e1070cce0515cfdd4b647f04b5b21f4fd4bdb44
Higor Barbosa <hlbarbosa@stefanini.com>
2019-10-18 16:41:17 -0300

    OpenAPI incluido.

---
+++ rsnugfunctions/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using AzureFunctions.Extensions.Swashbuckle;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Startup;
+
+[assembly: WebJobsStartup(typeof(SwashBuckleStartup))]
+namespace Startup
+{
+	internal class SwashBuckleStartup : IWebJobsStartup
+	{
+		public void Configure(IWebJobsBuilder builder)
+		{
+			//Register the extension
+			builder.AddSwashBuckle(Assembly.GetExecutingAssembly());
+
+		}
+	}
+}
